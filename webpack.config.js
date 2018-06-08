815099fddfd0b70d8dc97d394016e0761cbc6d9f
Pascal Sancho <psancho.13@gmail.com>
2018-06-08 11:06:33 +0200

    COSMETIC removed console.logs & added comments for env param

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,12 @@ const envs = {
 const configCommon = require('./build-utils/webpack.common.js');
 const configEnv = require(`./build-utils/webpack.${envs[TARGET]}.js`);
 
-module.exports = (env) => {
-    console.log('env: ', env);
+// function pattern is for future skills.
+// a parameter can be passed through the scripts section in package.json:
+// "start": "webpack-serve --env.prop1=value1 --env.prop2=value2"
+// will pass:
+// env: {prop1: "value1", prop2: "value2"}
+module.exports = (/*env*/) => {
     const config = merge(configCommon, configEnv);
-    console.log('config: ', config);
     return config;
 }
